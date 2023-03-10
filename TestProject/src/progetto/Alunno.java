483d03f2f9ff6d20ffe0cecbaba329fbcec56a0b
Marco <Marco@LAPTOP-KSFJPCHT>
2023-03-10 19:27:48 +0100

    Insert attributes, getter,setter,construttor to class Alunno

--- TestProject/src/progetto/Alunno.java
+++ TestProject/src/progetto/Alunno.java
@@ -1,6 +1,32 @@
 package progetto;
 
 public class Alunno {
-
+	private String name;
+	private String surname;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	
+	public Alunno(String name, String surname) {
+		this.name = name;
+		this.surname = surname;
+	}
+	
+	@Override
+	public String toString() {
+		return "Alunno [name=" + name + ", surname=" + surname + "]";
+	}
+	
+	
 	
 }
--- TestProject/src/progetto/Main.java
+++ TestProject/src/progetto/Main.java
@@ -1,11 +1,22 @@
 package progetto;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Ciao Mondo");
-		int i = 0;
+		
+		Scanner in = new Scanner(System.in);
+		System.out.println("Nuovo Alunno");
+		
+		System.out.println("nome:");
+		String name = in.nextLine();
+		System.out.println("cognome:");
+		String surname = in.nextLine();
+
+		Alunno alumn1 = new Alunno(name,surname);
+		System.out.println(alumn1);
 	}
 
 }
